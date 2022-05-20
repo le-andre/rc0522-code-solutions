b40d2a2a871c6fa0a4fbd9523f015b8197c2dad0
Andre Le <logisticx@yahoo.com>
2022-05-20 19:21:17 +0000

    completed array-manipulation-1

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,4 +1,6 @@
 /* exported last */
+/* When the function Last is called with the parameter array,
+the for loop will start from the last index of the array and return that last element of the array. */
 function last(array) {
   var lastResult = array[(array.length - 1)];
   for (var i = array.length - 1; i > array.length; i--) {
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,4 +1,7 @@
 /* exported tail */
+/* When the function Tail is called with the parameter array, the for loop will
+start counting from index [1] and those elements will be pushed into a new array. */
+
 function tail(array) {
   var tailResult = [];
   for (var i = 1; i < array.length; i++) {

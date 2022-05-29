a0db76246b3c99bc058ac505443a6f8c4436f8c4
Andre Le <logisticx@yahoo.com>
2022-05-29 23:49:20 +0000

    added pseudo code

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,4 +1,8 @@
 /* exported drop */
+/* when the function drop is called with two parameters, array and count, with the iterations of
+a for loop, the slice method of the array object with the values of count as the starting value, and it will end
+at the iteration of the loop, is assigned to a new variable. The return value of that variable
+should be a new array containing elements of that array after the count elements. */
 function drop(array, count) {
   for (var i = 0; i <= array.length; i++) {
     var value = array.slice(count, i);
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,4 +1,8 @@
 /* exported includes */
+/* when the function includes is called with two parameters, array and value,
+in the opening codeblock we have a forloop that will loop through the index of
+the array if it's equal to the value parameter. if the array's index is equal to the value
+parameter, return true, else, return false . */
 function includes(array, value) {
   for (var i = 0; i <= array.length; i++) {
     var valueOf;

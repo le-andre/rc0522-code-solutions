088229701fce67d7952e8c8b4c81c0e8b630d19a
Andre Le <logisticx@yahoo.com>
2022-06-07 19:04:32 +0000

    completed javascript-constructors

---
+++ javascript-constructors/example-constructor.js
@@ -0,0 +1,10 @@
+function ExampleConstructor() {
+
+}
+console.log(ExampleConstructor);
+console.log(typeof ExampleConstructor);
+
+var exampleConstructorReturn = new ExampleConstructor();
+console.log(exampleConstructorReturn);
+
+console.log(exampleConstructorReturn instanceof ExampleConstructor);
---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var object = new Object();
+console.log(object);
+console.log(typeof object);
+
+var array = new Array();
+console.log(array);
+console.log(typeof array);
+
+var aFunction = new Function();
+console.log(aFunction);
+console.log(typeof aFunction);
+
+var string = new String();
+console.log(string);
+console.log(typeof string);
+
+var number = new Number();
+console.log(number);
+console.log(typeof number);
+
+var boolean = new Boolean();
+console.log(boolean);
+console.log(typeof boolean);

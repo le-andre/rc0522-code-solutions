c12835f1b83a46c7d9dd7803f0dabe7e2152815e
Andre Le <logisticx@yahoo.com>
2022-06-29 21:54:40 +0000

    completed the-event-loop updated

--- the-event-loop/timeout.js
+++ the-event-loop/timeout.js
@@ -2,10 +2,3 @@ console.log('Hello, just a moment...');
 setTimeout(() => {
   console.log('Thanks for waiting!');
 }, '2000');
-setInterval(() => {
-  let currentCount = 3;
-  for (let i = currentCount; i >= 0; i--) {
-    currentCount--;
-    console.log(i);
-  }
-}, '1000');

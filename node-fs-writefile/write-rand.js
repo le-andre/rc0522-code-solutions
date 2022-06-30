87bc857d246093adb13bf5a35a5f9fb676c20a85
Andre Le <logisticx@yahoo.com>
2022-06-30 08:45:35 +0000

    completed node-fs-writefile

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -2,5 +2,4 @@ const data = new Uint8Array(Buffer.from(Math.random() + '\n'));
 const fs = require('fs');
 fs.writeFile('random.txt', data, err => {
   if (err) throw err;
-  // console.log(data);
 });

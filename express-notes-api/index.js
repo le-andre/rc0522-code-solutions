af5884eedf16b37fb53659bdb8579c83b7c4ced9
Andre Le <logisticx@yahoo.com>
2022-07-07 18:45:29 +0000

    complleted sql-insert

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -51,5 +51,68 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (id < 1) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+    return;
+  }
+  if (typeof dataObject.notes[id] === 'undefined') {
+    res.status(404).json({
+      error: 'cannot find note with id: ' + id
+    });
+    return;
+  }
+  delete dataObject.notes[id];
+  fs.writeFile('data.json', JSON.stringify(dataObject, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error has occured'
+      });
+    } else {
+      res.sendStatus(204);
+    }
+  });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (id < 1) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+    return;
+  }
+  const newObject = req.body;
+  if (typeof newObject.content === 'undefined') {
+    res.status(400).json({
+      error: 'content is a required field'
+    });
+    return;
+  }
+  if (typeof dataObject.notes[id] === 'undefined') {
+    res.status(404).json({
+      error: 'cannot find note with id: ' + id
+    });
+    return;
+  }
+  newObject.id = id;
+  dataObject.notes[id] = newObject;
+  dataObject.nextId++;
+  fs.writeFile('data.json', JSON.stringify(dataObject, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error has occured'
+      });
+    } else {
+      res.status(200).json(newObject);
+    }
+  });
+});
+
 // eslint-disable-next-line no-console
 app.listen(3000, console.log('listening at 3000!'));

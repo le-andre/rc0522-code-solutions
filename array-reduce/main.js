92e201e929d8ab779588db9ccb2190826c85a0dc
Andre Le <logisticx@yahoo.com>
2022-07-11 23:34:42 +0000

    completed array reduce

---
+++ array-reduce/main.js
@@ -0,0 +1,31 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+const z = 0;
+const i = 1;
+const sum = numbers.reduce((x, y) => x + y, z);
+console.log(sum);
+
+const product = numbers.reduce((x, y) => x * y, i);
+console.log(product);
+
+const balance = account.reduce((x, y) => y.type === 'deposit' ? x + y.amount : x - y.amount, z);
+console.log(balance);
+
+const composite = traits.reduce((x, y) => Object.assign(x, y));
+console.log(composite);

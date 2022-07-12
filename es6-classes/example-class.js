ecd4aded759c03ec69a900000863c484b5caa1e9
Andre Le <logisticx@yahoo.com>
2022-07-12 18:07:17 +0000

    wip

---
+++ es6-classes/example-class.js
@@ -0,0 +1,26 @@
+function ExampleClass() {
+
+}
+
+const example = new ExampleClass();
+// class ExampleClass {
+//   constructor(example) {
+//     this.example = example;
+//   }
+
+// //   example() {
+// //     return ExampleClass();
+// //   }
+// // }
+// // const example = new ExampleClass();
+
+console.log('typeof example:', typeof example);
+console.log('value of example:', example);
+
+console.log('typeof example.__proto__', typeof example.__proto__);
+console.log('value of example.__proto__', example.__proto__);
+
+console.log('typeof ExampleClass.prototype:', typeof ExampleClass.prototype);
+console.log('value of ExampleClass.prototype:', ExampleClass.prototype);
+
+console.log('example instanceof ExampleClass', example instanceof ExampleClass);

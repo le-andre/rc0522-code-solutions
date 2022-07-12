b0e89dd7b5c0ec551ba1a3e54eeb9272068faedc
Andre Le <logisticx@yahoo.com>
2022-07-12 00:41:49 +0000

    added notes

--- array-reduce/main.js
+++ array-reduce/main.js
@@ -21,11 +21,27 @@ const i = 1;
 const sum = numbers.reduce((x, y) => x + y, z);
 console.log('sum:', sum);
 
+// const add = (x, y) =>{
+//   console.log('x', x);
+//   console.log('y',y)
+//   return x+y
+// };
+// const sum = numbers.reduce(add, 0) 0 = index
+
 const product = numbers.reduce((x, y) => x * y, i);
 console.log('product:', product);
 
 const balance = account.reduce((x, y) => y.type === 'deposit' ? x + y.amount : x - y.amount, z);
 console.log('balance:', balance);
-
+// const net = (balance, transaction) => {
+//   console.log('Balance:', balance);
+//   console.log('Transaction:', transaction);
+//   console.log('-------------');
+//   if (transaction.type === 'deposit') {
+//     return balance + transaction.amount;
+//   }
+//   return balance - transaction.amount;
+// };
+// const balance = account.reduce(net,0) initialize with 0
 const composite = traits.reduce((x, y) => Object.assign(x, y));
 console.log('composite:', composite);

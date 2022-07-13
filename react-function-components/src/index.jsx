d4894d13e423971b29ad902068e3f84c5800bee1
Andre Le <logisticx@yahoo.com>
2022-07-13 22:01:29 +0000

    completed react-function-components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton() {
+  return <button>Click Me!</button>;
+}
+const element = CustomButton();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);

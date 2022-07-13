08e16ba07c838a32d80a5aadba8b094b390535a7
Andre Le <logisticx@yahoo.com>
2022-07-13 22:22:11 +0000

    implemented feedback

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 function CustomButton() {
   return <button>Click Me!</button>;
 }
-const element = CustomButton();
+const element = <CustomButton />;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);

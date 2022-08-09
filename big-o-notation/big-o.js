664807b9a25bdecb3fd4bbcae897de388876e51f
Andre Le <logisticx@yahoo.com>
2022-08-09 00:29:49 +0000

    fixed uniqueQuadratic answer

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -41,4 +41,4 @@ function uniqueQuadratic(words) {
     }
   }
   return unique;                    // 1 * 1 = O(1)
-} // Big O Notation for uniqueQuadratic: O(n2)
+} // Big O Notation for uniqueQuadratic: O(n^2)
